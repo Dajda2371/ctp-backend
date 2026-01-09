bb15ba0ed7bd5564723ca653bbda6e3c982e4214
David Beneš <david@davidbenes.cz>
2026-01-09 16:20:31 +0100

    allowed all methods

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.options.auth import login
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(login.router, prefix="/auth")
